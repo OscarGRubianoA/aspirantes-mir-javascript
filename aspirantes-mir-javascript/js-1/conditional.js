caf5655c3c6e9cfd4f009d98f9eb7e7befa9a697
BetoCochas <oscargio86@hotmail.com>
2023-03-12 22:34:26 -0500

    'solucionalosejerciciosjs'

---
+++ aspirantes-mir-javascript/js-1/conditional.js
@@ -0,0 +1,14 @@
+function calcularcolor(numero){
+    if (numero === 1){
+        console.log("El color es negro")
+    }
+    else if(numero === 2){
+        console.log("El color es blanco")
+    }
+    else if(numero === 3){
+        console.log("El color es rojo")
+    }
+    else{
+        console.log("El color es verde")
+    }
+}
---
+++ aspirantes-mir-javascript/js-1/sum.js
@@ -0,0 +1,9 @@
+function suma(numero) {
+    let resultado = 0;
+  
+    for (let i = 1; i <= numero; i++) {
+      resultado += i;
+    }
+  
+    return resultado;
+  }
